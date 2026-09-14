89487eb325e12711f90f59d7b5dff9889629ba3e
KayL-0080 <leeky1537@gmail.com>
2026-09-14 13:41:28 +0900

    refactor: update member roles list

--- components/tabs/MembersTab.js
+++ components/tabs/MembersTab.js
@@ -4,13 +4,13 @@ import { useState } from 'react';
 import styles from './tabs.module.css';
 
 const NTRP_OPTIONS = [1.0, 1.5, 2.0, 2.5, 3.0, 3.5, 4.0, 4.5, 5.0];
-const ROLE_OPTIONS = ['회장', '총무', '경기이사', '운영이사', '행사담당', '정회원', '준회원', '게스트'];
+const ROLE_OPTIONS = ['회장', '부회장', '총무', '경기이사', '정회원', '준회원', '게스트'];
 
 export default function MembersTab({ members, onUpdateLocal, onSave, onAdd, onDelete, isAdmin, currentClub, onBulkUpdateFeeStatus }) {
 
   const sortedMembers = [...members].sort((a, b) => {
     // 1. 특정 직책 상단 고정 및 준회원/게스트 하단 배치
-    const rolePriority = { '회장': 1, '총무': 2, '경기이사': 3, '운영이사': 4, '행사담당': 5, '준회원': 998, '게스트': 999 };
+    const rolePriority = { '회장': 1, '부회장': 2, '총무': 3, '경기이사': 4, '정회원': 10, '준회원': 998, '게스트': 999 };
     const pA = rolePriority[a.role] || 99;
     const pB = rolePriority[b.role] || 99;
     if (pA !== pB) return pA - pB;
